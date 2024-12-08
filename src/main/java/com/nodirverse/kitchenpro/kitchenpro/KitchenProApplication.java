a7f40431bebf0e09a16fda10765de711fa677421
nodirbekomonboyev <omonboevnodir77@gmail.com>
2024-12-09 02:16:02 +0500

    Initial commit

---
+++ src/main/java/com/nodirverse/kitchenpro/kitchenpro/KitchenProApplication.java
@@ -0,0 +1,13 @@
+package com.nodirverse.kitchenpro.kitchenpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitchenProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KitchenProApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nodirverse/kitchenpro/kitchenpro/KitchenProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nodirverse.kitchenpro.kitchenpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitchenProApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
